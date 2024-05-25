fda753ed6604f0a66a31351f54af5ee740c608cf
Kyle Kartchner <kkartch@gmail.com>
2024-05-24 18:46:39 -0600

    Reoganizing configs for nvim

--- neovim/config.nvim/lua/core/keymaps.lua
+++ neovim/config.nvim/lua/core/keymaps.lua
@@ -1,21 +1,3 @@
-vim.opt.backspace = '2'
-vim.opt.showcmd = true
-vim.opt.laststatus = 2
-vim.opt.autowrite = true
-vim.opt.cursorline = true
-vim.opt.autoread = true
-
--- Allow cut and paste between tmux and terminal sessions using yank and put
-vim.opt.clipboard = 'unnamed'
-
--- use spaces for tabs
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.shiftround = true
-vim.opt.expandtab = true
-
--- window settings
-vim.wo.number = true
 
 -- LazyGit
 vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>')
--- neovim/config.nvim/lua/core/preconfig.lua
+++ neovim/config.nvim/lua/core/preconfig.lua
@@ -1,12 +1,21 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function ()
-    vim.opt.shiftwidth = 2
-    vim.opt.tabstop = 2
-    vim.opt.softtabstop = 2
-  end,
-})
+-- Allow cut and paste between tmux and terminal sessions using yank and put
+vim.opt.clipboard = 'unnamed'
+
+-- use spaces for tabs
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+-- window settings
+vim.wo.number = true
